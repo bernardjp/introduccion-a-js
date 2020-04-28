60babacdbb22926713ae924d9807329350c672e8
Juan Pablo Bernard <bernardjp@users.noreply.github.com>
2020-04-28 16:58:51 -0300

    Agrega funcion para shufflear array
    Es una funcion basada en el algoritmo Fisher-Yates para mezclar el contenido de un array.

--- tareas/clase-10/memotest.js
+++ tareas/clase-10/memotest.js
@@ -1,1 +1,31 @@
 
+//Algorithmo Fisher-Yates para hacer un shuffle de un Array.
+
+const arrayColores = [
+    'background-color: red',
+    'background-color: red',
+    'background-color: orangered',
+    'background-color: orangered',
+    'background-color: yellow',
+    'background-color: yellow',
+    'background-color: green',
+    'background-color: green',
+    'background-color: darkcyan',
+    'background-color: darkcyan',
+    'background-color: darkblue',
+    'background-color: darkblue',
+    'background-color: blueviolet',
+    'background-color: blueviolet',
+];
+
+function shuffleArray (array){
+    let indiceActual = array.length, indiceRandom, valorTemporal;
+
+    for (i = indiceActual - 1; i > 0; i--){
+        indiceRandom = Math.floor(Math.random() * (i + 1));
+        valorTemporal = array[i];
+        array[i] = array[indiceRandom];
+        array[indiceRandom] = valorTemporal;
+    }
+    return array;
+}
