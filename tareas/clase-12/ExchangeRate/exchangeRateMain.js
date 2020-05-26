11db423ecd011defff60a3a06c70e4875e367750
Juan Pablo Bernard <bernardjp@users.noreply.github.com>
2020-05-26 17:42:44 -0300

    Agrega estilos a la página

--- tareas/clase-12/ExchangeRate/exchangeRateMain.js
+++ tareas/clase-12/ExchangeRate/exchangeRateMain.js
@@ -3,18 +3,13 @@ const $selectBaseCurrency = document.getElementById('input-currency');
 const $startButton = document.getElementById('apply-filters');
 const $resetButton = document.getElementById('reset-filters');
 const $tableBody = document.getElementById('table-rates');
+const $tableTitle = document.getElementById('table-title');
+
+const $inputDate = document.getElementById('input-date');
+const currentDate = new Date().toISOString().substring(0, 10);
 
 let currencyBase,
     currencyRatesList;
-
-//  BOTON DE INICIO:
-//1- Tomar los parametros de los inputs.
-//  1a- validar valores?    
-//2- Crear y enviar el request a la API.
-//  3- Crear una lista con los valores retornados de la API.
-//  3a- Crear una pantalla para una respuesta positva.
-//  3b- Crear una pantalla para una respuesta negativa.
-//4- Resetar la lista si se cargan valores nuevos en los inputs.
 
 $startButton.onclick = function exchangeRateList(event) {
     let baseCurrencyInput = document.getElementById('input-currency').value;
@@ -28,8 +23,8 @@ $startButton.onclick = function exchangeRateList(event) {
 };
 
 $resetButton.onclick = function resetPage(event) {
+    $inputDate.value = currentDate;
     resetDisplay(); 
-    resetInputs();
 
     event.preventDefault();
 }
@@ -48,24 +43,29 @@ function apiResponseHandler(currency, date){
 };
 
 function createDisplay(apiResponse){
-    console.log('respuesta:', apiResponse);
     document.getElementById('table-container').classList.remove('hidden');
     $tableBody.innerHTML = '';
 
+    const baseCurrency = apiResponse.base;
     const completeList = apiResponse.rates;
     const currenciesList = Object.keys(completeList);
     const ratesList = Object.values(completeList);
+
+    document.querySelector('#base-currency').innerText = baseCurrency;
+    document.querySelector('#date').innerText = $inputDate.value;
 
     currenciesList.forEach((currency, index) => {
         const $tr = document.createElement('tr');
         const $tdCurrency = document.createElement('td');
         const $tdRate = document.createElement('td');
 
-        $tdCurrency.innerText = currency;
-        $tdRate.innerText = ratesList[index];
-        $tr.appendChild($tdCurrency);
-        $tr.appendChild($tdRate);
-        $tableBody.appendChild($tr);
+        if(currency !== baseCurrency){
+            $tdCurrency.innerText = currency;
+            $tdRate.innerText = ratesList[index];
+            $tr.appendChild($tdCurrency);
+            $tr.appendChild($tdRate);
+            $tableBody.appendChild($tr); 
+        }
     })
 };
 
@@ -73,11 +73,6 @@ function resetDisplay(){
     document.getElementById('table-container').classList.add('hidden');
     document.getElementById('table-rates').innerHTML = '';
 };
-
-function resetInputs(){
-    dateInputSetup();
-};
-
 
 //Setup inicial de la landingPage.
 fetch('https://api.exchangeratesapi.io/latest')
@@ -108,10 +103,8 @@ function defaultListCurrency(currenciesList){
 }
 
 (function dateInputSetup() {
-    const currentDate = new Date().toISOString().substring(0, 10);
-    const inputDate = document.getElementById('input-date');
-    inputDate.value = currentDate;
-    inputDate.setAttribute('min', '1999-01-04');
-    inputDate.setAttribute('max', currentDate);
+    $inputDate.value = currentDate;
+    $inputDate.setAttribute('min', '1999-01-04');
+    $inputDate.setAttribute('max', currentDate);
 })()
 
