fda182a25b2970b2cca059cd76aa69a78d241b1b
Juan Pablo Bernard <bernardjp@users.noreply.github.com>
2020-07-17 20:16:23 -0300

    Agrega renderizado de recurso 'Pokemon'
    Renderiza el componente 'Pokemon' con data de la API o del localStorage. Pendiente: componentes de los recursos restantes.

--- tareas/clase-12/Pokedex/src/ui/ui-card.js
+++ tareas/clase-12/Pokedex/src/ui/ui-card.js
@@ -1,12 +1,62 @@
-
 export function renderResourceCard(resourceURL, resourceType) {
-    console.log(resourceURL);
-    console.log(resourceType);
-    // Cada recurso se renderiza de manera diferente. Esta funcion invoca al componente correspondiente:
-    // ie: 
-    /*  renderPokemonCard(resourceURL);
-        renderBerryCard(resourceURL);
-        renderItemCard(resourceURL);
-        renderMoveCard(resourceURL);
-    */
+  if (resourceType === 'pokemon') { renderPokemonCard(resourceURL); }
+
+  if (resourceType === 'berry') { renderBerryCard(resourceURL); }
+
+  if (resourceType === 'item') { renderItemCard(resourceURL); }
+
+  if (resourceType === 'move') { renderMoveCard(resourceURL); }
+}
+
+function renderPokemonCard(resourceURL) {
+  renderCardImage(resourceURL.sprites.front_default);
+
+  const $cardPropertiesList = document.getElementById('card-properties');
+  $cardPropertiesList.innerHTML = `
+        <li>Name: <strong>${resourceURL.name.toUpperCase()}</strong></li>
+        <li>Poké-ID: <strong>${resourceURL.id}</strong></li>
+        <li>Type: <strong>${resourceURL.types[0].type.name.toUpperCase()}</strong></li>
+        <li>First Appereance: <strong>POKÉMON ${resourceURL.game_indices[0].version.name.toUpperCase()}</strong></li>
+        <li>Height: <strong>${resourceURL.height}'</strong></li>
+        <li>Weight: <strong>${resourceURL.weight}lb</strong></li>
+        <hr>
+        <p>Combat Stats</p>
+        <li>Hit Points: <strong>${resourceURL.stats[0].base_stat}</strong></li>
+        <li>Attack: <strong>${resourceURL.stats[1].base_stat}</strong></li>
+        <li>Defense: <strong>${resourceURL.stats[2].base_stat}</strong></li>
+        <li>Special Attack: <strong>${resourceURL.stats[3].base_stat}</strong></li>
+        <li>Special Defense: <strong>${resourceURL.stats[4].base_stat}</strong></li>
+        <li>Speed: <strong>${resourceURL.stats[5].base_stat}</strong></li>
+        <hr>
+        <p>Abilities</p>
+        <li>Ability A: <strong>${resourceURL.abilities[0].ability.name.toUpperCase()}</strong></li>
+        <li>Ability B: <strong>${resourceURL.abilities[1].ability.name.toUpperCase()}</strong></li>
+    `;
+}
+
+function renderBerryCard(resourceURL) {
+  const $cardPropertiesList = document.getElementById('card-properties');
+  renderCardImage('./img/psyduck.png');
+  $cardPropertiesList.innerHTML = '<p>CONTENT CURRENTLY UNAVAILABLE</p>';
+  console.log(resourceURL.name);
+}
+
+function renderItemCard(resourceURL) {
+  const $cardPropertiesList = document.getElementById('card-properties');
+  renderCardImage('./img/psyduck.png');
+  $cardPropertiesList.innerHTML = '<p>CONTENT CURRENTLY UNAVAILABLE</p>';
+  console.log(resourceURL.name);
+}
+
+function renderMoveCard(resourceURL) {
+  const $cardPropertiesList = document.getElementById('card-properties');
+  renderCardImage('./img/psyduck.png');
+  $cardPropertiesList.innerHTML = '<p>CONTENT CURRENTLY UNAVAILABLE</p>';
+  console.log(resourceURL.name);
+}
+
+function renderCardImage(imgURL) {
+  const $cardImage = document.querySelector('.img');
+  $cardImage.src = `${imgURL}`;
+  $cardImage.classList.remove('hidden');
 }
--- tareas/clase-12/Pokedex/src/utils/utils.js
+++ tareas/clase-12/Pokedex/src/utils/utils.js
@@ -1,25 +1,45 @@
+export function pageIndexCalculator(paginationURLs, resourceCount) {
+  const [previousPage, nextPage] = paginationURLs;
+  let pageTotalIndex;
+  let pageCurrentIndex;
 
-export function pageIndexCalculator(nextPaginationURL, resourceCount) {
-    const resourcesPerPageLimit = 20;
-    const pageTotalIndex = Math.ceil(resourceCount / resourcesPerPageLimit);
-    let pageCurrentIndex;
+  if (nextPage) {
+    const pageParameters = pageParametersCalculator(nextPage);
+    pageTotalIndex = Math.ceil(resourceCount / pageParameters.limit);
+    pageCurrentIndex = pageParameters.offset / pageParameters.limit;
+  } else {
+    const pageParameters = pageParametersCalculator(previousPage);
+    pageTotalIndex = Math.ceil(resourceCount / pageParameters.limit);
+    pageCurrentIndex = pageTotalIndex;
+  }
 
-    if (nextPaginationURL) {    
-        const resourcesPerPageOffset = nextPaginationURL.match(/\d+/g)[1];
-        pageCurrentIndex = resourcesPerPageOffset / resourcesPerPageLimit;
-    } else {
-        pageCurrentIndex = pageTotalIndex;
-    }
+  const pageIndex = {
+    current: pageCurrentIndex,
+    total: pageTotalIndex,
+  };
 
-    let pageIndex = {
-        current: pageCurrentIndex,
-        total: pageTotalIndex
-    }
-
-    return pageIndex;
+  return pageIndex;
 }
 
 export function resourceIDCalculator(resourceURL) {
-    const resourceID = resourceURL.match(/\d+/g)[1];
-    return resourceID;
+  const resourceID = resourceURL.match(/\d+/g)[1];
+  return resourceID;
+}
+
+export function pageParametersCalculator(url) {
+  let pageOffset;
+  let pageLimit;
+
+  if (url) {
+    pageOffset = url.match(/offset=([0-9]+)/).pop();
+    pageLimit = url.match(/limit=([0-9]+)/).pop();
+  } else {
+    pageOffset = undefined;
+    pageLimit = undefined;
+  }
+
+  return {
+    offset: pageOffset,
+    limit: pageLimit,
+  };
 }
